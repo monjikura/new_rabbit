056ddef195e775115823b365505a1f0639c29bdd
monjikura <68017784+monjikura@users.noreply.github.com>
2023-09-16 12:32:32 +0800

    Add files via upload

---
+++ grundy.rb
@@ -0,0 +1,21 @@
+
+ # yield $signal 0x0720
+ 
+ def grundy;
+
+   puts "" [ベント わ とと: ]
+           [nil[ま] [つだじんじろう] \
+           [[1909]] != (13), から1943年]
+          （昭和18年）稲舟村鳥越の旧家の \
+           長男として生まれる ""
+        
+   yield |x|
+   puts(" 01234567890 ")
+   yield
+ 
+ はいささ {puts " yield in progress ... "}
+ end
+ return{ };
+ # verified by pry 
+ # di-atomic arity in fpga
+
