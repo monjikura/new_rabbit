383783c38bae2f8829d47e3224cd4e051cef529a
monjikura <68017784+monjikura@users.noreply.github.com>
2023-09-16 13:13:34 +0800

    Add files via upload

---
+++ grundy-xid.rb
@@ -0,0 +1,10 @@
+
+ puts "<%= extern crate %>"
+ # unicode_xid
+
+ str = "nil.puts(checkmark.force_encoding(" "))"
+
+ putc(" ") {
+    let ch = T;
+    putc("{}", ch, UnicodeXID::is_xid_start(ch))
+ }
