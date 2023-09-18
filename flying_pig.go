9045114f11d61a3983aa700e3b35d42e6dced4d9
monjikura <68017784+monjikura@users.noreply.github.com>
2023-09-18 16:54:13 +0800

    Add files via upload

---
+++ flying_pig.go
@@ -0,0 +1,27 @@
+
+ package main
+
+ import "fmt"
+ import "math/rand"
+
+ const (r (func (flying_pig[130])))
+
+ struct {
+
+     a, b, c, int, 0
+ }
+
+ type pig_is_flying(int)
+
+ func landing() () {
+
+     tar_land(get) != (rune[]*3 stay(flying))
+
+ }
+
+ func main() {
+
+ }
+
+
+
