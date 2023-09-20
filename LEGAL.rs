b14f09569af1d34f0d1b064dd3a95ca28f96eac4
monjikura <68017784+monjikura@users.noreply.github.com>
2023-09-20 19:46:00 +0800

    Add files via upload

---
+++ LEGAL.rs
@@ -0,0 +1,110 @@
+
+/* a1447d60b5638209427521a9284fe75b  LEGAL.rs
+ Concept of sovereign immunity
+
+What is the general approach to the concept of sovereign immunity in your state?
+
+Malaysia subscribes to the common law doctrine of restrictive immunity.
+
+Legal basis
+
+What is the legal basis for the doctrine of sovereign immunity in your state?
+
+Articles 32(1) and 181(2) of the Federal Constitution provide the legal basis for immunity against the Rulers in Malaysia; however, legal proceedings may be commenced against the Rulers in the Special Court.
+
+As far as foreign sovereign immunity is concerned, the legal basis is section 3 of the Civil Law Act 1956, which applies the common law in Malaysia (Village Holdings v Her Majesty the Queen in Right of Canada [1988] 2 MLJ 656).
+
+Multilateral treaties
+
+Is your state a party to any multilateral treaties on sovereign immunity? Has the state made any reservations or declarations regarding the treaties?
+
+Even though the government has ratified a treaty that binds the government under international law, it has no domestic legal effect unless the legislature passes a law to give effect to that treaty. The following are some examples of statutes passed by Parliament to give effect to treaties concluded by Malaysia:
+
+the Recognition and Enforcement of Foreign Arbitral Awards Act 1985 to give effect to the provisions of the New York Convention 1958. This piece of national legislation remained in force until it was repealed by the Arbitration Act 2005 as the enabling instrument to recognise the application of the New York Convention in Malaysia;
+the Convention on Settlement of Investment Disputes Act 1966 to give legal effect to the Washington Convention 1965;
+the Geneva Conventions Act 1962, as revised in 1993, to give effect to the Four Geneva Conventions for the Protection of the Victims of War of 1949;
+the Diplomatic Privileges (Vienna Convention) Act 1966, as amended in 1999, to give effect to the Vienna Convention on Diplomatic Relations 1961;
+the Carriage by Air Act 1974, to give effect to the Warsaw Convention of 1929, as amended by the Hague Protocol of 1955 and the Guadalajara Convention of 1961;
+the Exclusive Economic Zone Act 1984, to give effect to certain provisions of the United Nations Convention on the Law of the Sea 1982;
+the International Organisations (Privileges and Immunities) Act 1992, to give effect to the Convention on the Privileges and Immunities of the United Nations 1946;
+the Consular Relations (Privileges and Immunities) Act 1999, to give effect to the Vienna Convention on Consular Relations 1963; and
+The Bilateral Investment Treaties with at least 71 countries around the globe, beginning with its first BIT signed with Germany on 22 December 1960.
+
+Jurisdictional immunity
+
+Domestic law
+
+Describe domestic law governing the scope of jurisdictional immunity.
+
+The scope of jurisdictional immunity depends on the international treaties entered by Malaysia. However, the international treaties will not have effect in Malaysia unless they are passed into domestic law by an act of Parliament.
+
+Immunity does not extend to commercial transactions.
+
+State waiver of immunity or consent
+
+How can the state, or its various organs and instrumentalities, waive immunity or consent to the exercise of jurisdiction?
+
+A state can waive jurisdictional immunity or consent to the exercise of jurisdiction by expressed submission to the jurisdiction of another state (eg, a party to an arbitration agreement or member of international treaties).
+
+Alternatively, jurisdictional immunity can be waived by an implied submission to jurisdiction (eg, by taking part in the arbitration proceedings without raising his or her privilege or making an application to the court to set aside the arbitration award) (Duff Development Company, Limited v Government of Kelantan and another Respondent [1924] AC 797).
+
+In which types of transactions or proceedings do states not enjoy immunity from suit (even without the state’s consent or waiver)? How does the law of your country assess whether a transaction falls into one of these categories?
+
+Jurisdictional immunity is limited to acts of government. It does not extend to commercial transactions entered by the state or sovereign (Hii Yii Ann v Deputy Commissioner of Taxation of the Commonwealth of Australia & Ors [2018] 7 MLJ 393).
+
+If one of the exceptions to sovereign immunity set out above applies, is there any related principle that could prevent a court having jurisdiction over the state?
+
+There are certain matters that are non-justiciable in Malaysia, set out in the Federal Constitution, State Constitution and Acts of Parliament. These are, for example, matters considered non-justiciable under the domestic law (ie, the Minister’s decision in certain Acts of Parliament, including any proceedings in Parliament); matters that fall within the prerogative of the Ruler; and matters relating to succession of Rulers.
+
+Proceedings against a state enterprise
+
+To what extent do proceedings against a state enterprise or similar entity affect the immunity enjoyed by the state? Is there precedent for piercing the corporate veil to subject the state itself to those proceedings?
+
+There is no precedent for piercing the corporate veil to subject the state to proceedings faced by a state enterprise. Conversely, state immunity has been extended to certain organisations and representatives of the organisations pursuant to the International Organisations (Privileges and Immunities) Act 1992.
+
+Standing
+
+What is the nexus the plaintiff needs to have standing to bring a claim against a state?
+
+There is no case law or provision that prescribes the nexus required for the plaintiff to bring a claim against the state. From a legal perspective, the plaintiff will have the nexus so long as he or she is party to the commercial transaction with the state.
+
+Nexus of forum court
+
+What is the nexus the forum court requires to exercise jurisdiction over a state if the property or conduct that forms the subject of the claim is outside the forum state’s territory?
+
+The forum court would have to establish that there are sufficient connecting factors (eg, the whether the parties are domicile of that country, the currency used in the transaction and where the contract was entered into) to that particular country for it to be the appropriate country to exercise jurisdiction over the dispute.
+
+Interim or injunctive relief
+
+When a state is subject to proceedings before a court or arbitral tribunal in your jurisdiction, what interim or injunctive relief is available?
+
+Section 11 of the Arbitration Act 2005 provides a list of interim remedies available to parties to an arbitration. Pursuant to section 5 of the Arbitration Act 2005, the federal and state government is bound by the Arbitration Act 2005.
+
+However, the interim remedies available under section 11 of the Arbitration Act 2005 is limited by section 29(1) of the Government Proceeding Act 1956: the courts do not have the power to grant an injunction against the government but may in lieu thereof make an order declaratory of the rights of the parties.
+
+This is applicable to the federal and state government. There does not appear to be a corresponding legislation to cater for foreign states.
+
+Final relief
+
+When a state is subject to proceedings before a court or arbitral tribunal in your jurisdiction, what type of final relief is available?
+
+Pursuant to section 29(1) of the Government Proceeding Act 1956, the court shall have the power to make all such orders as it has in a matter between private citizens. However, the court shall not grant an injunction, make an order for specific performance, or order the recovery of land or property against the state, but may in lieu thereof make a declaration of the rights of the parties.
+
+Service of process
+
+Identify the court or other entity that must be served with process before any proceeding against a state may be issued.
+
+If a treaty relationship exists with a foreign country, the relevant bodies to be served would be identified in the treaties (ie, under the Hague Convention, the service of process is by sending a request to a central authority).
+
+For example, in respect of mutual assistance for criminal matters, the designated authority is the Attorney General of Malaysia.
+
+How is process served on a state?
+
+A process can only be served on the state when the sovereign ruler or government submits to the jurisdiction of the court (Village Holdings v Her Majesty the Queen in Right of Canada [1988] 2 MLJ 656).
+
+Judgment in absence of state participation
+
+Under what conditions will a judgment be made against a state that does not participate in proceedings?
+
+There is no case law or provision governing this point. However, as process can only be served on the state when the sovereign or government submit to the jurisdiction of the court, it is unlikely that the court may make a substantive judgment against a state that does not participate in the proceedings.
+*/
