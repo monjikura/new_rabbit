b0ca135d1962c47735d7dfc7d2da48dbe1d4c237
monjikura <68017784+monjikura@users.noreply.github.com>
2023-09-21 16:36:40 +0800

    Add files via upload

---
+++ felony.rb
@@ -0,0 +1,6 @@
+
+ #![only::feloney::r#in::zsh::market]
+ o130::localn!{"girl_pee_standing"}
+ baez!{"...felicia.feloney"} 
+ println!{"...dead police at fish market"}
+ println!{"...zsh was here before Perl"}
