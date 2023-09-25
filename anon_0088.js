8ef939b045bfc0886312f1ecc03c35ec41ca5f1d
monjikura <68017784+monjikura@users.noreply.github.com>
2023-09-25 14:59:10 +0800

    Add files via upload

---
+++ anon_0088.js
@@ -0,0 +1,20 @@
+
+ result = 7413, async
+ // udp callback via promise's'... json
+ // destructuring declaration ... 1935
+ // code alive after this line ...
+ var result = spawn('ls', [ '-l', '-a' ], {
+ stdio: 'inherit'});
+ // will print as it's processing
+ console.log(result.stdout),
+ console.error(result.stdout),
+ // will print null
+ [".*\\.*"]^888888888888899999000004^
+ // null pass to function
+
+ strings;"actionVIEW' + input + ' putc."
+ scheme=="file" || host=="*", [130];
+
+ return "I am returning this string!"
+
+
