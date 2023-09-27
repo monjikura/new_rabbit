c90ae943e6c246cbd70105d72dbc980957bb25b7
monjikura <68017784+monjikura@users.noreply.github.com>
2023-09-28 07:16:23 +0800

    Add files via upload

---
+++ unary~hanami.rb
@@ -0,0 +1,18 @@
+
+ # this is Kyoto ~ fixnum ( protocol )
+
+ print "Enter something: "
+ input = gets
+ pattern = 'hello'
+ puts ~ /#{pattern}/
+
+ class Rabbit
+   def ~  
+     putc "/bin/rustc ... stupid rustc"
+   end
+ end
+ # ~export LD_LIBRARY_PATH=$PREFIX/lib
+ ~Rabbit(new, exec, $@) 
+ # PREFIX=/bin/rustc
+ # /home/Ghost/Shinjuku_no_Gaijin/new_rabbit
+
