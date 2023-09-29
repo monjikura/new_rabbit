d8b18055ccab61ae95bc0b08ca354ef2d449704e
monjikura <68017784+monjikura@users.noreply.github.com>
2023-09-29 18:05:53 +0800

    Add files via upload

---
+++ girl_107_China.py
@@ -0,0 +1,11 @@
+
+# md5 is 3d823367bf7d04a4bbcbf1b178948424
+print ("PEP 3333 Python Gateway 0day @@130")
+yield a(130, []);
+for n in 0, 1100000000000000000000000000000:
+    return sum(map(lambda x: x*x*x))
+    # we start stealing here ...
+    map[:3333, :1032, :0.285974, :x| 107]
+    # warning ... python map is bss.data
+
+print (sys.getsizeof(a))
