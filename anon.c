6142d421ed4c60a9c9dc0f8d6a031be24d8554b8
monjikura <68017784+monjikura@users.noreply.github.com>
2023-10-04 18:43:03 +0800

    Add files via upload

---
+++ anon.c
@@ -0,0 +1,15 @@
+
+ /* printf -version anon.c
+    ... you have issued a valid, working ioctl
+    call to the wrong device ...
+    are you a dog ? ... ?? */
+
+ #include<stdio.h>
+ #include<stdlib.h>
+
+ # G-d is a black cat
+
+ void this(typewriter _2727)
+
+ // use kernel be stupid srand call nice girl
+ // ... give me give me icecream
